c177e0d391fecf3fad3b2a4ca3702f574d8e6551
Simon Biber <simon@srkb.net>
2019-10-27 17:28:10 +1030

    Initial Commit

---
+++ coredatademo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  coredatademo
+//
+//  Created by Simon Biber on 27/10/19.
+//  Copyright © 2019 Simon Biber. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
